c3e363f0ea4dfbe24eb67930d89baed392b94ec8
diginxz <rodrigodonizetevideira@gmail.com>
2023-02-14 15:59:11 -0300

    add aula

---
+++ AppCEP/AppCEP/Model/Cidade.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCEP.Model
+{
+    public class Cidade
+    {
+            public int id_cidade { get; set; }
+            public string descricao { get; set; }
+            public string uf { get; set; }
+            public string codigo_ibge { get; set; }
+            public string ddd { get;}
+    }
+}
---
+++ AppCEP/AppCEP/Service/DataService.cs
@@ -0,0 +1,27 @@
+﻿using AppCEP.Model;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCEP.Service
+{
+    public class DataService
+    {
+        public static async Task<Endereco> GetEnderecoByCep(string cep)
+        {
+            Endereco end;
+            using (HttpClient client= new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync("https://cep.metoda.com.br/endereco/by-cep?cep=" + cep);
+                
+                if (response.IsSuccessStatusCode)
+                { 
+                    string json = await response.Content.ReadAsStringAsync().Result;
+                }
+            }
+        }
+    }
+}
